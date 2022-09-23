69072fae3cbcfd2266964b43f67c13f3437b958e
AlamNurul95 <nurulalamsm95@gmail.com>
2022-09-23 11:48:09 +0100

    4th commit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,73 @@
-document.getElementById('16gb-memory-btn').addEventListener('click', function () {
+function getMemoryBtn(isfree) {
     const memoryCost = document.getElementById('memory-cost');
-    const memoryTotal = parseFloat(memoryCost.innerText);
-    memoryCost.innerText = memoryTotal + 100;
+    const PreviousMemoryTotal = parseFloat(memoryCost.innerText);
+    let newMemoryTotal;
+
+    if (isfree == true) {
+        newMemoryTotal = 0;
+    }
+    else {
+        newMemoryTotal = 100;
+    }
+    memoryCost.innerText = newMemoryTotal;
+}
+function storageBtn(btnName) {
+    const storageCost = document.getElementById('storage-cost');
+    const previousStorageTotal = parseFloat(storageCost.innerText);
+    let newStorageTotal;
+    if (btnName == 'storage-256gb') {
+        newStorageTotal = 0;
+
+    }
+    else if (btnName == 'storage-512gb') {
+        newStorageTotal = 250;
+    }
+    else if (btnName == 'storage-1tb') {
+        newStorageTotal = 500;
+    }
+    storageCost.innerText = newStorageTotal;
+
+}
+function deliveryUpdate(isfree) {
+    const deliveryCost = document.getElementById('del-charge');
+    let delCostValue = parseFloat(deliveryCost.innerText);
+    if (isfree == true) {
+        delCostValue = 0;
+    }
+    else {
+        delCostValue = 20;
+    }
+    deliveryCost.innerText = delCostValue;
+}
+//8gb memory set
+document.getElementById('8gb-memory-btn').addEventListener('click', function () {
+    getMemoryBtn(true);
 });
+//16gb Memory set
+document.getElementById('16gb-memory-btn').addEventListener('click', function () {
+    getMemoryBtn(false);
+});
+//256 gb storage
+document.getElementById('storage-256gb').addEventListener('click', function () {
+    storageBtn('storage-256gb');
+})
+//512 gb storage
+document.getElementById('storage-512gb').addEventListener('click', function () {
+    storageBtn('storage-512gb');
+});
+//1tb storage
+document.getElementById('storage-1tb').addEventListener('click', function () {
+    storageBtn('storage-1tb');
+});
+//delivery
+document.getElementById('free').addEventListener('click', function () {
+    deliveryUpdate(true);
+});
+document.getElementById('delivery-cost').addEventListener('click', function () {
+    deliveryUpdate(false);
+});
+
+
+
+
 
